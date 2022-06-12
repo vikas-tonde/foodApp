e364dc4a1a0ade7fc1f832dc7a7f248472a0caca
Vikas <tondev23@gmail.com>
2022-06-12 20:54:58 +0530

    Login done...

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 import express from "express";
 import bodyParser from 'body-parser';
 import usersRoutes from "./routes/users.js";
+import cors from "cors";
+import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
+import dotenv from 'dotenv'
+
+dotenv.config()
+mongoose.connect(process.env.MONGO_URL, () => {
+    console.log("Database connected");
+});
 
 const app = express();
 const PORT = 5000;
 
-app.use(bodyParser.json());
-app.use("/users",usersRoutes);
+// var corsOptions = {
+//     origin: `http://localhost:${PORT}`
 
-app.listen(PORT, ()=> console.log(`Server is runnning on port : http://localhost:${PORT}`));
+// }
+
+app.use(cookieParser());
+app.use(cors({
+    credentials: true,
+    origin: "*"
+}));
+app.use(bodyParser.json());
+app.use("/users", usersRoutes);
+
+app.listen(PORT, () => console.log(`Server is runnning on port : http://localhost:${PORT}`));
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,77 @@
 import express from "express";
+import bcrypt from 'bcryptjs';
+import { User } from '../models/users.js'
+
+import pkg from "jsonwebtoken";
+const Jwt = pkg;
 
 const router = express.Router();
 
-router.get('/',(req,res)=>{
-    res.send("Hello");
+router.get('/', async (req, res) => {
+    try {
+        const cookie = req.cookies['jwt'];
+        const claims = Jwt.verify(cookie, "secret");
+        if (!claims) {
+            return res.status(401).send({
+                messaage: "Unauthenticated"
+            });
+        }
+        const user = await User.findOne({ _id: claims._id });
+        const { password, ...data } = await user.toJSON();
+        res.send(data);
+    } catch (e) {
+        return res.status(401).send({
+            messaage: "Unauthenticated"
+        });
+    }
+});
+
+router.post('/register', (req, res) => {
+    (async function () {
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(req.body.password, salt);
+        const user = new User({
+            name: req.body.name,
+            email: req.body.email,
+            password: hashedPassword
+        })
+        const result = await user.save();
+        const { password, ...data } = result.toJSON();
+        res.send(data);
+    })().catch(err => {
+        res.status(424).send({
+            messaage: "Register unsuccessfull"
+        });
+    });
+});
+
+router.post("/login", async (req, res) => {
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+        return res.status(404).send({
+            messaage: "user not found"
+        });
+    }
+    if (!await bcrypt.compare(req.body.password, user.password)) {
+        return res.status(400).send({
+            messaage: "Invalid Credentials"
+        });
+    }
+    const token = Jwt.sign({ _id: user._id }, "secret")
+    res.cookie('jwt', token, {
+        httpOnly: true,
+        maxAge: 12 * 60 * 60 * 1000
+    });
+    res.send({
+        message: "success"
+    });
+});
+
+router.post("/logout", (req, res) => {
+    res.cookie('jwt', '', { maxAge: 0 });
+    res.send({
+        messaage: "You have been logged out"
+    });
 });
 
 export default router;
