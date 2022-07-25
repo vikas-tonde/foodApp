0803cc9fd7b970265492525fa2cf9d530703f0b3
Vikas <tondev23@gmail.com>
2022-07-25 20:50:11 +0530

    Recipient Accept donation done

--- index.js
+++ index.js
@@ -9,13 +9,14 @@ import usersRoutes from "./routes/users.js";
 import donationRoutes from './routes/donations.js';
 import recipientRoutes from './routes/recipients.js'
 import requireAuth from './middleware/authMiddleware.js';
-import donorAuth from './middleware/donorMiddleware.js'
+import donorAuth from './middleware/donorMiddleware.js';
+import recipientAuth from "./middleware/recipientMiddleware.js";
 
 dotenv.config()
 
 mongoose.connect(process.env.MONGO_URL, () => {
     console.log("Database connected");
-}); 
+});
 const app = express();
 const PORT = process.env.PORT;
 
@@ -27,6 +28,6 @@ app.use(cors({
 app.use(bodyParser.json());
 app.use("/users", usersRoutes);
 app.use("/donation", requireAuth, donorAuth, donationRoutes);
-app.use('/recipient',requireAuth,recipientRoutes);
+app.use('/recipient', requireAuth, recipientAuth, recipientRoutes);
 
 app.listen(PORT, () => console.log(`Server is runnning on port : http://localhost:${PORT}`));
--- routes/recipients.js
+++ routes/recipients.js
@@ -8,7 +8,7 @@ const router = new express.Router();
 router.get("/", async (req, res) => {
     const pageSize = 9;
     var pagenumber = req.query.page;
-    var result = await Donation.find({recipient:{$exists:false}})
+    var result = await Donation.find({ recipient: { $exists: false } })
         .limit(pageSize)
         .skip((pagenumber - 1) * pageSize)
         .exec();
@@ -18,5 +18,19 @@ router.get("/", async (req, res) => {
     });
 });
 
+router.post("/accept", async (req, res) => {
+    /*
+    donationId:Id of the donation,
+    id:id of the recipient
+    
+    */
+    var query = { '_id': req.body.donationId, 'recipient': { $exists: false } };
+    var recipient = { 'recipient': req.body.id }
+    Donation.findOneAndUpdate(query, recipient, function (err, doc) {
+        if (err)
+            return res.send(500, { error: "Failed to allocate" });
+        return res.send("Successfully Accepted... Please go to the location and receive.");
+    });
+});
 
 export default router;
